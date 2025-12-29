d33a6b9f9d028794314ad545a05a444bf1349a54
Federico Corna <federicorna@icloud.com>
2025-12-29 12:23:27 +0100

    creazione progetto

---
+++ Assets/_Script/Player/PlayerControler.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private Rigidbody2D _rb;
+    private float h, v;
+    private Vector2 _dir; 
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        _dir = new Vector2 (h, v).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition (_rb.position + _dir * (_speed * Time.fixedDeltaTime));
+    }
+
+}
